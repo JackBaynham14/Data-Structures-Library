534f008a9c6881407d00419fc365ef9154ba3d35
JackBaynham14 <jack_baynham@outlook.com>
2021-01-11 22:36:09 -0700

    Fix Binary Tree insert method;

--- BinaryTrees.py
+++ BinaryTrees.py
@@ -19,6 +19,8 @@ def insert(root, val):
         return TreeNode(val)
 
     if val > root.val:
-        return insert(root.right, val)
+        root.right = insert(root.right, val)
     else:
-        return insert(root.left, val)
+        root.left = insert(root.left, val)
+    
+    return root
